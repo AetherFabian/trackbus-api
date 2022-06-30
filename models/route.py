735c46b071950d2f68628d8e209919f803bcadcf
AetherFabian <fabian_galarza@hotmail.com>
2022-06-30 13:15:21 -0600

    feat: get by bus_id

--- models/route.py
+++ models/route.py
@@ -1,13 +1,23 @@
+from urllib import response
 import database
+from base import Base
 
-class Route():
+class Route(Base):
     
     def get_route(bus_id):
-        response = database.db.routes.find_one(bus_id)
+        response = database.db.routes.find_one({'bus_id': bus_id})
+        response.pop('_id')
+        print(response, bus_id)
         return response
     
-    def put_route():
-        pass
-    
-    def delete_route():
-        pass
+    def put_route(bus_id):
+        
+        if Base.check_if_id_exists(bus_id):
+            response = database.db.routes.find_one_and_update()
+            return response
+        
+            
+    def delete_route(bus_id):
+        if Base.check_if_id_exists(bus_id):
+            response = database.db.routes.find_one_and_delete()
+            return response

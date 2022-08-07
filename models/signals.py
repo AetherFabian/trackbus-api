9145efb52b2ee321e7318a070005a9865ad16ad7
AetherFabian <fabian_galarza@hotmail.com>
2022-08-07 17:12:35 -0600

    fix: add time zone in spoted at

--- models/signals.py
+++ models/signals.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from re import sub
+from dateutil import tz
 import database
 
 class Signals():
@@ -16,9 +16,6 @@ class Signals():
         response_stops = database.db.stops.find({}, {'stop_id': 1, 'cordinates_x': 1, 'cordinates_y': 1, 'name_stop':1})
         signals = []
         stops = []
-        estimated_times = []
-        return_signals = []
-        estimate = 0
         
         for stop in response_stops:
             del stop['_id']
@@ -27,27 +24,14 @@ class Signals():
             del signal['_id']
             signals.append(signal)
             
-        for i in range(len(stops)):
-            data = []
-            data_sum = 0
-            for h in range(len(signals)):
-                if stops[i]['stop_id'] == signals[h]['stop_id']:
-                    #transformar fechas a horas
-                    data.append(signals[h]['spoted_at'])
-            # 
-            data_sum = max(data) - min(data)
-            estimated_times.append(data_sum)
-            
         for x in range(len(stops)):
             for j in reversed(range(len(signals))):
                 if signals[j]['stop_id'] == stops[x]['stop_id']:
-                    return_signals+=[{'cordinates_x': stops[x]['cordinates_x'], 'cordinates_y': stops[x]['cordinates_y'], 'spoted_at': signals[j]['spoted_at'], 'name_stop': stops[x]['name_stop'], 'estimated_time': estimated_times[estimate]}]
-                    estimate +=1
+                    return_signals+=[{'cordinates_x': stops[x]['cordinates_x'], 'cordinates_y': stops[x]['cordinates_y'], 'spoted_at': signals[j]['spoted_at'][17:25], 'name_stop': stops[x]['name_stop']}]
                     break            
 
         return return_signals
     
     def post_signal(self, request):
-        request['spoted_at'] = datetime.now()
+        request['spoted_at'] = datetime.now(tz=tz.gettz('America/Chihuahua'))
         return database.db.signals.insert_one(request)
-        
